6d5c818a4892291938a4f2602aa54c6ac4d377eb
Jesse Navarette <j_navarette@hotmail.com>
2023-12-10 18:05:04 -0800

    added stater code

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,51 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+//wait for the DOM is ready
+$(document).ready(function(){
+  //get current date and time using Day.js
+  const currentDate = dayjs().format('MMMM, D, YYYY');
+  $("#currentDay").text(currentDate);
+
+  //create time blocks for standar bussines hours(9am-5pm)
+  const workHours = Array.from({ length: 12 }, (_, index) => index + 9);
+
+  console.log(workHours)
+
+  //funtion to create time blocks
+  function generateTimeBlocks(){
+      const $main = $("main");
+
+      workHours.forEach( (hour)=>{
+          const $timeBlock = $("<div>").addClass("time-block");
+          const $hour = $("<div>").addClass("hour").text(`${hour}:00`);
+          const $textArea = $("<textarea>").addClass("description");
+
+          const currentHour = dayjs().hour();
+
+          if(hour < currentHour){
+              $timeBlock.addClass("past");
+          }else if(hour === currentHour){
+              $timeBlock.addClass("present");
+          }else{
+              $timeBlock.addClass("future");
+          }
+
+          //check local storage for saved events and display
+          const savedEvent = localStorage.getItem(`event-${hour}`);
+          if(savedEvent){
+              $textArea.val(savedEvent);
+          }
+
+          const $saveBtn = $("<button>").addClass("saveBtn").text("save");
+          $timeBlock.append($hour, $textArea, $saveBtn);
+          $main.append($timeBlock);
+      })
+  }
+  generateTimeBlocks();
+
+  //save event to local storage
+  $("fa-save").on("click", function(){
+      const hour = $(this).siblings(".hour").text().split(":")[0];
+      const event = $(this).siblings(".description").val();
+      localStorage.setItem(`event-${hour}`, event);
+  })
+
+})
